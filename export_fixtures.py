687c8cfe778dcb40661d7f23eb1cd26d7bd6894c
Akbarali Salohiddinov <akbaralisalohiddinov808@gmail.com>
2025-02-27 10:20:32 +0500

    add new items to gitignore, change export_fixtures (now added 'exclude-fields' for models)

--- export_fixtures.py
+++ export_fixtures.py
@@ -17,13 +17,23 @@ FIXTURE_DIR = os.path.join(settings.BASE_DIR, 'fixtures')
 os.makedirs(FIXTURE_DIR, exist_ok=True)
 
 
-def export_to_json(model, filename):
+def export_to_json(model, filename, exclude_fields=None):
     """
-    Model ma'lumotlarini JSON fixture sifatida saqlaydi (formatlangan)
+    Model ma'lumotlarini JSON fixture sifatida saqlaydi (formatlangan),
+    lekin exclude_fields ro‘yxatidagi maydonlarni saqlamaydi.
     """
+    exclude_fields = exclude_fields or []
+
     file_path = os.path.join(FIXTURE_DIR, filename)
     raw_json = serialize('json', model.objects.all())
+
     formatted_json = json.loads(raw_json)
+
+    # Unwanted fields removal
+    for item in formatted_json:
+        for field in exclude_fields:
+            item["fields"].pop(field, None)
+
     formatted_json_str = json.dumps(formatted_json, indent=4, ensure_ascii=False)
 
     with open(file_path, 'w', encoding='utf-8') as f:
@@ -32,9 +42,12 @@ def export_to_json(model, filename):
     print(f"{filename} fayli yaratildi!")
 
 
-export_to_json(Student, 'students.json')
-export_to_json(Sponsor, 'sponsors.json')
-export_to_json(Donation, 'donations.json')
-export_to_json(University, 'universities.json')
-
-print("🎉 Barcha ma'lumotlar JSON formatda saqlandi!")
+# There we can exclude fields, which are not needle for fixtures
+try:
+    export_to_json(Student, 'students.json', exclude_fields=['updated_at', 'created_at'])
+    export_to_json(Sponsor, 'sponsors.json', exclude_fields=['updated_at', 'created_at'])
+    export_to_json(Donation, 'donations.json', exclude_fields=['updated_at', 'created_at'])
+    export_to_json(University, 'universities.json')
+    print("Barcha kerakli ma'lumotlar JSON formatida saqlandi!")
+except Exception as e:
+    print(f"Error: {e}")
